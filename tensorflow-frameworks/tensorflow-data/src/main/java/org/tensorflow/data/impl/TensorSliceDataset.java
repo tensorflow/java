1ea6339d7f203dced9bfc86791030e9e14bcb973
Dhruv Rajan <dhruv@krishnaprem.com>
2019-12-28 12:13:06 -0800

    adding take and skip dataset

--- tensorflow-frameworks/tensorflow-data/src/main/java/org/tensorflow/data/impl/TensorSliceDataset.java
+++ tensorflow-frameworks/tensorflow-data/src/main/java/org/tensorflow/data/impl/TensorSliceDataset.java
@@ -12,36 +12,22 @@ import java.util.stream.Collectors;
 
 public class TensorSliceDataset extends Dataset {
   private org.tensorflow.op.data.TensorSliceDataset dataset;
-  private List<Shape> outputShapes;
-  private List<DataType<?>> outputTypes;
 
   public TensorSliceDataset(Ops tf, List<Operand<?>> components, List<DataType<?>> outputTypes) {
-    super(tf);
+    super(tf, outputTypes,
+        components.stream()
+            .map(c -> Utils.tail(c.asOutput().shape()))
+            .collect(Collectors.toList()));
 
     if (!(components.size() == outputTypes.size())) {
       throw new IllegalArgumentException("Lists `tensors` and `dtypes` must have the same number of elements.");
     }
 
-    List<Shape> outputShapes = components.stream().map(c -> Utils.tail(c.asOutput().shape()))
-        .collect(Collectors.toList());
-
-    this.outputTypes = outputTypes;
-    this.outputShapes = outputShapes;
-    this.dataset = tf.data.tensorSliceDataset(components, outputShapes);
+    this.dataset = tf.data.tensorSliceDataset(components, this.getOutputShapes());
   }
 
   @Override
   public Operand<?> getVariant() {
     return dataset;
-  }
-
-  @Override
-  public List<DataType<?>> getOutputTypes() {
-    return outputTypes;
-  }
-
-  @Override
-  public List<Shape> getOutputShapes() {
-    return outputShapes;
   }
 }
