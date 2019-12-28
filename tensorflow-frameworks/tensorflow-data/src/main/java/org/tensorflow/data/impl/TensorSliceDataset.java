fca73ff129ca67a9f920bac64b5e9e34301c4e08
Dhruv Rajan <dhruv@krishnaprem.com>
2019-12-28 01:41:17 -0800

    TensorFlow Data examples with batching working in graph / eager mode

--- tensorflow-frameworks/tensorflow-data/src/main/java/org/tensorflow/data/impl/TensorSliceDataset.java
+++ tensorflow-frameworks/tensorflow-data/src/main/java/org/tensorflow/data/impl/TensorSliceDataset.java
@@ -1,5 +1,6 @@
 package org.tensorflow.data.impl;
 
+import org.tensorflow.DataType;
 import org.tensorflow.Operand;
 import org.tensorflow.tools.Shape;
 import org.tensorflow.data.Dataset;
@@ -10,35 +11,37 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 public class TensorSliceDataset extends Dataset {
-    private org.tensorflow.op.data.TensorSliceDataset dataset;
-    private List<Shape> outputShapes;
-    private List<Class<?>> outputTypes;
+  private org.tensorflow.op.data.TensorSliceDataset dataset;
+  private List<Shape> outputShapes;
+  private List<DataType<?>> outputTypes;
 
-    public TensorSliceDataset(Ops tf, List<Operand<?>> components, List<Class<?>> outputTypes) {
-        if (!(components.size() == outputTypes.size())) {
-            throw new IllegalArgumentException("Lists `tensors` and `dtypes` must have the same number of elements.");
-        }
+  public TensorSliceDataset(Ops tf, List<Operand<?>> components, List<DataType<?>> outputTypes) {
+    super(tf);
 
-        List<Shape> outputShapes = components.stream().map(c -> Utils.tail(c.asOutput().shape()))
-                .collect(Collectors.toList());
-
-        this.outputTypes = outputTypes;
-        this.outputShapes = outputShapes;
-        this.dataset = tf.data.tensorSliceDataset(components, outputShapes);
+    if (!(components.size() == outputTypes.size())) {
+      throw new IllegalArgumentException("Lists `tensors` and `dtypes` must have the same number of elements.");
     }
 
-    @Override
-    public Operand<?> getVariant() {
-        return dataset;
-    }
+    List<Shape> outputShapes = components.stream().map(c -> Utils.tail(c.asOutput().shape()))
+        .collect(Collectors.toList());
 
-    @Override
-    public List<Class<?>> getOutputTypes() {
-        return outputTypes;
-    }
+    this.outputTypes = outputTypes;
+    this.outputShapes = outputShapes;
+    this.dataset = tf.data.tensorSliceDataset(components, outputShapes);
+  }
 
-    @Override
-    public List<Shape> getOutputShapes() {
-        return outputShapes;
-    }
+  @Override
+  public Operand<?> getVariant() {
+    return dataset;
+  }
+
+  @Override
+  public List<DataType<?>> getOutputTypes() {
+    return outputTypes;
+  }
+
+  @Override
+  public List<Shape> getOutputShapes() {
+    return outputShapes;
+  }
 }
