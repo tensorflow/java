d3077d16dbde1bed0e8446031a3da689094159be
Dhruv Rajan <dhruv@krishnaprem.com>
2019-12-29 18:11:24 -0800

    added tests for all dataset types (skip, take, batch)

--- tensorflow-frameworks/tensorflow-data/src/test/java/org/tensorflow/data/DatasetTest.java
+++ tensorflow-frameworks/tensorflow-data/src/test/java/org/tensorflow/data/DatasetTest.java
@@ -1,34 +1,16 @@
 package org.tensorflow.data;
 
-import org.junit.Before;
 import org.junit.Test;
 import org.tensorflow.*;
 import org.tensorflow.op.Ops;
 import org.tensorflow.types.TInt32;
 
-import java.nio.IntBuffer;
 import java.util.Arrays;
 import java.util.List;
 
 import static org.junit.Assert.assertArrayEquals;
-import static org.junit.Assert.assertEquals;
 
-public class DatasetTester {
-  int[][] testMatrix1;
-  int[][] testMatrix2;
-  @Before
-  public void setUp() {
-    testMatrix1 = new int[][]{
-        {1, 2, 3, 4, 5},
-        {2, 4, 6, 8, 10},
-        {3, 6, 8, 12, 15},
-        {4, 8, 12, 16, 20}
-    };
-
-    testMatrix2 = new int[][] {
-        {1}, {0}, {1}, {1}
-    };
-  }
+public class DatasetTest extends DatasetTestBase {
 
   @Test
   public void testEagerDatasetIterator() {
@@ -41,7 +23,7 @@ public class DatasetTester {
             Arrays.asList(TInt32.DTYPE, TInt32.DTYPE));
 
     int count = 0;
-    for(List<Output<?>> components : dataset) {
+    for (List<Output<?>> components : dataset) {
       Tensor<TInt32> batch1 = components.get(0).tensor().expect(TInt32.DTYPE);
       Tensor<TInt32> batch2 = components.get(1).tensor().expect(TInt32.DTYPE);
 
@@ -74,11 +56,11 @@ public class DatasetTester {
 
         int count = 0;
         while (true) {
-          try  {
+          try {
             List<Tensor<?>> outputs = session.runner()
-              .fetch(components.get(0))
-              .fetch(components.get(1))
-              .run();
+                .fetch(components.get(0))
+                .fetch(components.get(1))
+                .run();
 
             Tensor<TInt32> matrix1 = outputs.get(0).expect(TInt32.DTYPE);
             Tensor<TInt32> matrix2 = outputs.get(1).expect(TInt32.DTYPE);
@@ -87,16 +69,13 @@ public class DatasetTester {
             assertArrayEquals(testMatrix2[count], getIntTensorAsArray(matrix2));
 
             count++;
-          } catch (IndexOutOfBoundsException e) { break; }
+          } catch (IndexOutOfBoundsException e) {
+            break;
+          }
         }
       }
-
     }
   }
 
-  public static int[] getIntTensorAsArray(Tensor<TInt32> intTensor) {
-    IntBuffer buffer = IntBuffer.allocate((int) intTensor.shape().size());
-    intTensor.writeTo(buffer);
-    return buffer.array();
-  }
+
 }
