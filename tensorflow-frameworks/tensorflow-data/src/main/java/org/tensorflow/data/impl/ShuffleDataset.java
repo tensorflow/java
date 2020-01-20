77de569c20b7565df12dc63e2c63d736b8575b95
Dhruv Rajan <dhruv@krishnaprem.com>
2020-01-19 20:14:48 -0800

    fix dataset issues; comment keras files

--- tensorflow-frameworks/tensorflow-data/src/main/java/org/tensorflow/data/impl/ShuffleDataset.java
+++ tensorflow-frameworks/tensorflow-data/src/main/java/org/tensorflow/data/impl/ShuffleDataset.java
@@ -1,27 +1,27 @@
-package org.tensorflow.data.impl;
+// package org.tensorflow.data.impl;
 
-import org.tensorflow.DataType;
-import org.tensorflow.Operand;
-import org.tensorflow.data.Dataset;
-import org.tensorflow.op.Ops;
-import org.tensorflow.op.core.Constant;
-import org.tensorflow.tools.Shape;
-import org.tensorflow.types.TInt64;
+// import org.tensorflow.DataType;
+// import org.tensorflow.Operand;
+// import org.tensorflow.data.Dataset;
+// import org.tensorflow.op.Ops;
+// import org.tensorflow.op.core.Constant;
+// import org.tensorflow.tools.Shape;
+// import org.tensorflow.types.TInt64;
 
-import java.util.List;
+// import java.util.List;
 
-public class ShuffleDataset extends Dataset {
-  private org.tensorflow.op.data.ShuffleDataset shuffleDataset;
+// public class ShuffleDataset extends Dataset {
+//   private org.tensorflow.op.data.ShuffleDataset shuffleDataset;
 
-  public ShuffleDataset(Ops tf, Operand<?> variant, Constant<TInt64> bufferSize,
-                        Constant<TInt64> seed, Constant<TInt64> seed2,
-                        List<DataType<?>> outputTypes, List<Shape> outputShapes) {
-    super(tf, outputTypes, outputShapes);
-    this.shuffleDataset = tf.data.shuffleDataset(variant, bufferSize, seed, seed2, outputTypes, outputShapes);
-  }
+//   public ShuffleDataset(Ops tf, Operand<?> variant, Constant<TInt64> bufferSize,
+//                         Constant<TInt64> seed, Constant<TInt64> seed2,
+//                         List<DataType<?>> outputTypes, List<Shape> outputShapes) {
+//     super(tf, outputTypes, outputShapes);
+//     this.shuffleDataset = tf.data.shuffleDataset(variant, bufferSize, seed, seed2, outputTypes, outputShapes);
+//   }
 
-  @Override
-  public Operand<?> getVariant() {
-    return shuffleDataset;
-  }
-}
+//   @Override
+//   public Operand<?> getVariant() {
+//     return shuffleDataset;
+//   }
+// }
