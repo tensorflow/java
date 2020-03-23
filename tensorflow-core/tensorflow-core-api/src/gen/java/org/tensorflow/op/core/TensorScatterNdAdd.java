ac2bb9e5a0c93b36461a05300b6e582a92f91b19
Ashutosh Hathidara <ashutoshhathidara98@gmail.com>
2020-03-23 16:16:59 +0530

    Removed unnecessary examples for shape attribute which is not there in tensor_scatter_nd_add

--- tensorflow-core/tensorflow-core-api/src/gen/java/org/tensorflow/op/core/TensorScatterNdAdd.java
+++ tensorflow-core/tensorflow-core-api/src/gen/java/org/tensorflow/op/core/TensorScatterNdAdd.java
@@ -37,18 +37,6 @@ import org.tensorflow.types.family.TType;
  * are added onto an existing tensor (as opposed to a variable). If the memory
  * for the existing tensor cannot be re-used, a copy is made and updated.
  * <p>
- * `indices` is an integer tensor containing indices into a new tensor of shape
- * `shape`.  The last dimension of `indices` can be at most the rank of `shape`:
- * <p>
- *     indices.shape[-1] <= shape.rank
- * <p>
- * The last dimension of `indices` corresponds to indices into elements
- * (if `indices.shape[-1] = shape.rank`) or slices
- * (if `indices.shape[-1] < shape.rank`) along dimension `indices.shape[-1]` of
- * `shape`.  `updates` is a tensor with shape
- * <p>
- *     indices.shape[:-1] + shape[indices.shape[-1]:]
- * <p>
  * The simplest form of tensor_scatter_add is to add individual elements to a
  * tensor by index. For example, say we want to add 4 elements in a rank-1
  * tensor with 8 elements.
