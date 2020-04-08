2230843f9023df369f9ac14627f4509da70807ef
Dhruv Rajan <dhruv@krishnaprem.com>
2020-04-08 00:00:51 -0700

    use try-with-resources for test tensors

--- tensorflow-frameworks/tensorflow-data/src/test/java/org/tensorflow/data/SkipDatasetTest.java
+++ tensorflow-frameworks/tensorflow-data/src/test/java/org/tensorflow/data/SkipDatasetTest.java
@@ -9,31 +9,31 @@ import org.tensorflow.types.TInt32;
 import java.util.Arrays;
 import java.util.List;
 
-import static org.junit.Assert.assertArrayEquals;
 import static org.junit.Assert.assertEquals;
 
 public class SkipDatasetTest extends DatasetTestBase {
-  @Test
-  public void testEagerSkipDataset() {
-    Ops tf = Ops.create();
+    @Test
+    public void testEagerSkipDataset() {
+        Ops tf = Ops.create();
 
-    Dataset dataset = Dataset
-        .fromTensorSlices(tf,
-            Arrays.asList(
-                tf.constant(testMatrix1),
-                tf.constant(testMatrix2)),
-            Arrays.asList(TInt32.DTYPE, TInt32.DTYPE))
-        .skip(2);
+        Dataset dataset = Dataset
+                .fromTensorSlices(tf,
+                        Arrays.asList(
+                                tf.constant(testMatrix1),
+                                tf.constant(testMatrix2)),
+                        Arrays.asList(TInt32.DTYPE, TInt32.DTYPE))
+                .skip(2);
 
-    int count = 2;
-    for (List<Output<?>> components : dataset) {
-      Tensor<TInt32> batch1 = components.get(0).tensor().expect(TInt32.DTYPE);
-      Tensor<TInt32> batch2 = components.get(1).tensor().expect(TInt32.DTYPE);
-
-      assertEquals(testMatrix1.get(count), batch1.data());
-      assertEquals(testMatrix2.get(count), batch2.data());
-      count++;
+        int count = 2;
+        for (List<Output<?>> components : dataset) {
+            try (Tensor<TInt32> batch1 =
+                         components.get(0).tensor().expect(TInt32.DTYPE);
+                 Tensor<TInt32> batch2 = components.get(1).tensor().expect(TInt32.DTYPE);) {
+                assertEquals(testMatrix1.get(count), batch1.data());
+                assertEquals(testMatrix2.get(count), batch2.data());
+                count++;
+            }
+        }
     }
-  }
 
 }
