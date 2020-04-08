4dc76580f31651b60fda783ec911bb12fc2e4d3d
Dhruv Rajan <dhruv@krishnaprem.com>
2020-04-07 21:13:25 -0700

    Using NdArray in tests

--- tensorflow-frameworks/tensorflow-data/src/test/java/org/tensorflow/data/DatasetIteratorTest.java
+++ tensorflow-frameworks/tensorflow-data/src/test/java/org/tensorflow/data/DatasetIteratorTest.java
@@ -4,36 +4,16 @@ import org.junit.Before;
 import org.junit.Test;
 import org.tensorflow.*;
 import org.tensorflow.op.Ops;
+import org.tensorflow.tools.ndarray.IntNdArray;
+import org.tensorflow.tools.ndarray.StdArrays;
 import org.tensorflow.types.TInt32;
 
 import java.util.Arrays;
 import java.util.List;
 
-public class DatasetIteratorTest {
-    int[][] testMatrix1;
-    int[][] testMatrix2;
+import static org.junit.Assert.assertEquals;
 
-    @Before
-    public void setUp() {
-        testMatrix1 = new int[][]{
-                {1, 2, 3, 4, 5},
-                {2, 4, 6, 8, 10},
-                {3, 6, 8, 12, 15},
-                {4, 8, 12, 16, 20}
-        };
-
-        testMatrix2 = new int[][]{
-                {1}, {0}, {1}, {1}
-        };
-    }
-
-    @Test
-    public void getNext() {
-    }
-
-    @Test
-    public void makeInitializer() {
-    }
+public class DatasetIteratorTest extends DatasetTestBase{
 
     @Test
     public void fromStructure() {
@@ -61,6 +41,7 @@ public class DatasetIteratorTest {
             try (Session session = new Session(graph)) {
                 session.run(tf.init());
 
+                int count = 0;
                 while (true) {
                     try {
                         List<Tensor<?>> outputs = session.runner()
@@ -73,13 +54,10 @@ public class DatasetIteratorTest {
                              Tensor<TInt32> yBatch =
                                      outputs.get(1).expect(TInt32.DTYPE);) {
 
-                            System.out.println("New Batch:");
-                            System.out.println("features");
-                            XBatch.data().scalars().forEach(System.out::println);
-                            System.out.println("labels");
-                            yBatch.data().scalars().forEach(System.out::println);
+                            assertEquals(testMatrix1.get(count), XBatch.data());
+                            assertEquals(testMatrix2.get(count), yBatch.data());
 
-                            System.out.println();
+                            count++;
                         }
                     } catch (IndexOutOfBoundsException e) {
                         break;
@@ -105,19 +83,17 @@ public class DatasetIteratorTest {
 
         Dataset dataset = Dataset.fromTensorSlices(tf, tensors, dataTypes);
 
+        int count = 0;
         for (List<Output<?>> outputs : dataset) {
             try (Tensor<TInt32> XBatch =
                          outputs.get(0).tensor().expect(TInt32.DTYPE);
                  Tensor<TInt32> yBatch =
                          outputs.get(1).tensor().expect(TInt32.DTYPE);) {
 
-                System.out.println("New Batch:");
-                System.out.println("features");
-                XBatch.data().scalars().forEach(System.out::println);
-                System.out.println("labels");
-                yBatch.data().scalars().forEach(System.out::println);
+                assertEquals(testMatrix1.get(count), XBatch.data());
+                assertEquals(testMatrix2.get(count), yBatch.data());
 
-                System.out.println();
+                count++;
             }
         }
     }
