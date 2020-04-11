fa8f5d593423ea7beb6b18c83f73e87baffefb18
Dhruv Rajan <dhruv@krishnaprem.com>
2020-04-11 15:05:44 -0700

    use 2-space indentation

--- tensorflow-frameworks/tensorflow-data/src/main/java/org/tensorflow/data/Dataset.java
+++ tensorflow-frameworks/tensorflow-data/src/main/java/org/tensorflow/data/Dataset.java
@@ -20,184 +20,184 @@ import java.util.stream.Collectors;
  * allows iteration and transformations to be performed across these elements.
  */
 public abstract class Dataset implements Iterable<List<Output<?>>> {
-    protected Ops tf;
-    private List<DataType<?>> outputTypes;
-    private List<Shape> outputShapes;
+  protected Ops tf;
+  private List<DataType<?>> outputTypes;
+  private List<Shape> outputShapes;
 
-    public Dataset(Ops tf, List<DataType<?>> outputTypes, List<Shape> outputShapes) {
-        if (Objects.isNull(tf)) {
-            throw new IllegalArgumentException("Ops accessor cannot be null.");
-        } else if (outputTypes.size() != outputShapes.size()) {
-            throw new IllegalArgumentException("`outputTypes` and `outputShapes` must have the same size.");
-        }
-
-        this.tf = tf;
-        this.outputTypes = outputTypes;
-        this.outputShapes = outputShapes;
+  public Dataset(Ops tf, List<DataType<?>> outputTypes, List<Shape> outputShapes) {
+    if (Objects.isNull(tf)) {
+      throw new IllegalArgumentException("Ops accessor cannot be null.");
+    } else if (outputTypes.size() != outputShapes.size()) {
+      throw new IllegalArgumentException("`outputTypes` and `outputShapes` must have the same size.");
     }
 
-    /**
-     * Groups elements of this dataset into batches.
-     *
-     * @param batchSize     The number of desired elements per batch
-     * @param dropLastBatch Whether to leave out the final batch if it has fewer
-     *                      than `batchSize` elements.
-     * @return A batched Dataset
-     */
-    public final Dataset batch(long batchSize, boolean dropLastBatch) {
-        List<Shape> batchOutputShapes = getOutputShapes().stream()
-                .map(s -> Shape.of(batchSize, s.asArray()))
-                .collect(Collectors.toList());
+    this.tf = tf;
+    this.outputTypes = outputTypes;
+    this.outputShapes = outputShapes;
+  }
+
+  /**
+   * Groups elements of this dataset into batches.
+   *
+   * @param batchSize     The number of desired elements per batch
+   * @param dropLastBatch Whether to leave out the final batch if it has fewer
+   *                      than `batchSize` elements.
+   * @return A batched Dataset
+   */
+  public final Dataset batch(long batchSize, boolean dropLastBatch) {
+    List<Shape> batchOutputShapes = getOutputShapes().stream()
+        .map(s -> Shape.of(batchSize, s.asArray()))
+        .collect(Collectors.toList());
 
 
-        return new BatchDataset(tf, this.getVariant(), tf.constant(batchSize),
-                tf.constant(dropLastBatch), this.getOutputTypes(), batchOutputShapes);
+    return new BatchDataset(tf, this.getVariant(), tf.constant(batchSize),
+        tf.constant(dropLastBatch), this.getOutputTypes(), batchOutputShapes);
+  }
+
+  /**
+   * Groups elements of this dataset into batches.
+   * Includes the last batch, even if it has fewer than `batchSize` elements.
+   *
+   * @param batchSize The number of desired elements per batch
+   * @return A batched Dataset
+   */
+  public final Dataset batch(long batchSize) {
+    return batch(batchSize, false);
+  }
+
+  /**
+   * Returns a new `Dataset` which skips `count` initial elements from this
+   * dataset
+   *
+   * @param count The number of elements to `skip` to form the new dataset.
+   * @return A new Dataset with `count` elements removed.
+   */
+  public final Dataset skip(long count) {
+    return new SkipDataset(tf, this.getVariant(), tf.constant(count), this.getOutputTypes(), this.getOutputShapes());
+  }
+
+  /**
+   * Returns a new `Dataset` with only the first `count` elements from this
+   * dataset.
+   *
+   * @param count The number of elements to "take" from this dataset.
+   * @return A new Dataset containing the first `count` elements from this dataset.
+   */
+  public final Dataset take(long count) {
+    return new TakeDataset(tf, this.getVariant(), tf.constant(count), this.getOutputTypes(), this.getOutputShapes());
+  }
+
+  /**
+   * Creates an iterator which iterates through all batches of this Dataset in an eager fashion.
+   * Each batch is a list of components, returned as `Output` objects.
+   * <p>
+   * This method enables for-each iteration through batches when running
+   * in eager mode. For Graph mode batch iteration, see `makeOneShotIterator`.
+   *
+   * @return an Iterator through batches of this dataset.
+   */
+  @Override
+  public Iterator<List<Output<?>>> iterator() {
+
+    if (!(tf.scope().env() instanceof EagerSession)) {
+      throw new UnsupportedOperationException("Cannot iterate through a dataset in graph mode.");
     }
 
-    /**
-     * Groups elements of this dataset into batches.
-     * Includes the last batch, even if it has fewer than `batchSize` elements.
-     *
-     * @param batchSize The number of desired elements per batch
-     * @return A batched Dataset
-     */
-    public final Dataset batch(long batchSize) {
-        return batch(batchSize, false);
-    }
+    DatasetIterator iterator = makeOneShotIterator();
 
-    /**
-     * Returns a new `Dataset` which skips `count` initial elements from this
-     * dataset
-     *
-     * @param count The number of elements to `skip` to form the new dataset.
-     * @return A new Dataset with `count` elements removed.
-     */
-    public final Dataset skip(long count) {
-        return new SkipDataset(tf, this.getVariant(), tf.constant(count), this.getOutputTypes(), this.getOutputShapes());
-    }
+    return new Iterator<List<Output<?>>>() {
+      private DatasetOptional nextOptional = iterator.getNextAsOptional();
 
-    /**
-     * Returns a new `Dataset` with only the first `count` elements from this
-     * dataset.
-     *
-     * @param count The number of elements to "take" from this dataset.
-     * @return A new Dataset containing the first `count` elements from this dataset.
-     */
-    public final Dataset take(long count) {
-        return new TakeDataset(tf, this.getVariant(), tf.constant(count), this.getOutputTypes(), this.getOutputShapes());
-    }
+      @Override
+      public boolean hasNext() {
+        return nextOptional.hasValue().data().getBoolean();
+      }
 
-    /**
-     * Creates an iterator which iterates through all batches of this Dataset in an eager fashion.
-     * Each batch is a list of components, returned as `Output` objects.
-     * <p>
-     * This method enables for-each iteration through batches when running
-     * in eager mode. For Graph mode batch iteration, see `makeOneShotIterator`.
-     *
-     * @return an Iterator through batches of this dataset.
-     */
-    @Override
-    public Iterator<List<Output<?>>> iterator() {
+      @Override
+      public List<Output<?>> next() {
+        List<Output<?>> result = nextOptional.getValue();
+        nextOptional = iterator.getNextAsOptional();
+        return result;
+      }
+    };
+  }
 
-        if (!(tf.scope().env() instanceof EagerSession)) {
-            throw new UnsupportedOperationException("Cannot iterate through a dataset in graph mode.");
-        }
+  /**
+   * Creates a `DatasetIterator` that can be used to iterate
+   * over elements of this dataset.
+   * <p>
+   * This iterator will have to be initialized with a call
+   * to `iterator.makeInitializer(Dataset)` before elements
+   * can be retreived in a loop.
+   *
+   * @return A new `DatasetIterator` based on this dataset's structure.
+   */
+  public DatasetIterator makeInitializeableIterator() {
+    return DatasetIterator
+        .fromStructure(tf, outputTypes, outputShapes);
+  }
 
-        DatasetIterator iterator = makeOneShotIterator();
+  /**
+   * Creates a `DatasetIterator` that can be used to iterate over
+   * elements of this dataset. Using `makeOneShotIterator` ensures
+   * that the iterator is
+   * automatically initialized on this dataset.
+   * skips
+   * In graph mode, the initializer op will be added to the Graph's
+   * intitializer list, which must be run via `tf.init()`:
+   * <p>
+   * Ex:
+   * <pre>
+   *     try (Session session = new Session(graph) {
+   *         // Immediately run initializers
+   *         session.run(tf.init());
+   *     }
+   * </pre>
+   * <p>
+   * In eager mode, the initializer will be run automatically as a result
+   * of this call.
+   *
+   * @return A new `DatasetIterator` based on this dataset's structure.
+   */
+  public DatasetIterator makeOneShotIterator() {
+    DatasetIterator iterator = makeInitializeableIterator();
+    Op initializer = iterator.makeInitializer(this);
+    if (tf.scope().env() instanceof Graph) tf.initAdd(initializer);
+    return iterator;
+  }
 
-        return new Iterator<List<Output<?>>>() {
-            private DatasetOptional nextOptional = iterator.getNextAsOptional();
+  /**
+   * Creates an in-memory `Dataset` whose elements are slices of the given
+   * tensors. Each element of this dataset will be a List<Output<?>>,
+   * representing slices (e.g. batches) of the provided tensors.
+   *
+   * @param tf          Ops Accessor
+   * @param tensors     A list of Operand<?> representing components of this
+   *                    dataset (e.g. features, labels)
+   * @param outputTypes A list of `DataType` objects representing the data
+   *                    type of each component of this dataset.
+   * @return A new `Dataset`
+   */
+  public static Dataset fromTensorSlices(Ops tf, List<Operand<?>> tensors, List<DataType<?>> outputTypes) {
+    return new TensorSliceDataset(tf, tensors, outputTypes);
+  }
 
-            @Override
-            public boolean hasNext() {
-                return nextOptional.hasValue().data().getBoolean();
-            }
+  /**
+   * Get the variant tensor representing this dataset.
+   */
+  public abstract Operand<?> getVariant();
 
-            @Override
-            public List<Output<?>> next() {
-                List<Output<?>> result = nextOptional.getValue();
-                nextOptional = iterator.getNextAsOptional();
-                return result;
-            }
-        };
-    }
+  /**
+   * Get a list of output types for each component of this dataset.
+   */
+  public List<DataType<?>> getOutputTypes() {
+    return this.outputTypes;
+  }
 
-    /**
-     * Creates a `DatasetIterator` that can be used to iterate
-     * over elements of this dataset.
-     *
-     * This iterator will have to be initialized with a call
-     * to `iterator.makeInitializer(Dataset)` before elements
-     * can be retreived in a loop.
-     *
-     * @return A new `DatasetIterator` based on this dataset's structure.
-     */
-    public DatasetIterator makeInitializeableIterator() {
-        return DatasetIterator
-                .fromStructure(tf, outputTypes, outputShapes);
-    }
-
-    /**
-     * Creates a `DatasetIterator` that can be used to iterate over
-     * elements of this dataset. Using `makeOneShotIterator` ensures
-     * that the iterator is
-     * automatically initialized on this dataset.
-     *skips
-     * In graph mode, the initializer op will be added to the Graph's
-     * intitializer list, which must be run via `tf.init()`:
-     *
-     * Ex:
-     * <pre>
-     *     try (Session session = new Session(graph) {
-     *         // Immediately run initializers
-     *         session.run(tf.init());
-     *     }
-     * </pre>
-     *
-     * In eager mode, the initializer will be run automatically as a result
-     * of this call.
-     *
-     * @return A new `DatasetIterator` based on this dataset's structure.
-     */
-    public DatasetIterator makeOneShotIterator() {
-        DatasetIterator iterator = makeInitializeableIterator();
-        Op initializer = iterator.makeInitializer(this);
-        if (tf.scope().env() instanceof Graph) tf.initAdd(initializer);
-        return iterator;
-    }
-
-    /**
-     * Creates an in-memory `Dataset` whose elements are slices of the given
-     * tensors. Each element of this dataset will be a List<Output<?>>,
-     * representing slices (e.g. batches) of the provided tensors.
-     *
-     * @param tf Ops Accessor
-     * @param tensors A list of Operand<?> representing components of this
-     *                dataset (e.g. features, labels)
-     * @param outputTypes A list of `DataType` objects representing the data
-     *                    type of each component of this dataset.
-     * @return A new `Dataset`
-     */
-    public static Dataset fromTensorSlices(Ops tf, List<Operand<?>> tensors, List<DataType<?>> outputTypes) {
-        return new TensorSliceDataset(tf, tensors, outputTypes);
-    }
-
-    /**
-     * Get the variant tensor representing this dataset.
-     */
-    public abstract Operand<?> getVariant();
-
-    /**
-     * Get a list of output types for each component of this dataset.
-     */
-    public List<DataType<?>> getOutputTypes() {
-        return this.outputTypes;
-    }
-
-    /**
-     * Get a list of shapes for each component of this dataset.
-     */
-    public List<Shape> getOutputShapes() {
-        return this.outputShapes;
-    }
+  /**
+   * Get a list of shapes for each component of this dataset.
+   */
+  public List<Shape> getOutputShapes() {
+    return this.outputShapes;
+  }
 }
--- tensorflow-frameworks/tensorflow-data/src/test/java/org/tensorflow/data/DatasetIteratorTest.java
+++ tensorflow-frameworks/tensorflow-data/src/test/java/org/tensorflow/data/DatasetIteratorTest.java
@@ -13,85 +13,85 @@ import static org.junit.Assert.assertEquals;
 
 public class DatasetIteratorTest extends DatasetTestBase {
 
-    @Test
-    public void testGraphIteration() {
-        try (Graph graph = new Graph()) {
-            Ops tf = Ops.create(graph);
+  @Test
+  public void testGraphIteration() {
+    try (Graph graph = new Graph()) {
+      Ops tf = Ops.create(graph);
 
-            List<Operand<?>> tensors = Arrays.asList(
-                    tf.constant(testMatrix1),
-                    tf.constant(testMatrix2)
-            );
+      List<Operand<?>> tensors = Arrays.asList(
+          tf.constant(testMatrix1),
+          tf.constant(testMatrix2)
+      );
 
-            List<DataType<?>> dataTypes = Arrays.asList(
-                    TInt32.DTYPE, TInt32.DTYPE
-            );
+      List<DataType<?>> dataTypes = Arrays.asList(
+          TInt32.DTYPE, TInt32.DTYPE
+      );
 
-            Dataset dataset =
-                    Dataset.fromTensorSlices(tf, tensors, dataTypes);
+      Dataset dataset =
+          Dataset.fromTensorSlices(tf, tensors, dataTypes);
 
-            DatasetIterator iterator = dataset.makeOneShotIterator();
+      DatasetIterator iterator = dataset.makeOneShotIterator();
 
-            List<Output<?>> components = iterator.getNext();
-            Operand<?> X = components.get(0);
-            Operand<?> y = components.get(1);
+      List<Output<?>> components = iterator.getNext();
+      Operand<?> X = components.get(0);
+      Operand<?> y = components.get(1);
 
-            try (Session session = new Session(graph)) {
-                session.run(tf.init());
+      try (Session session = new Session(graph)) {
+        session.run(tf.init());
 
-                int count = 0;
-                while (true) {
-                    try {
-                        List<Tensor<?>> outputs = session.runner()
-                                .fetch(X)
-                                .fetch(y)
-                                .run();
-
-                        try (Tensor<TInt32> XBatch =
-                                     outputs.get(0).expect(TInt32.DTYPE);
-                             Tensor<TInt32> yBatch =
-                                     outputs.get(1).expect(TInt32.DTYPE);) {
-
-                            assertEquals(testMatrix1.get(count), XBatch.data());
-                            assertEquals(testMatrix2.get(count), yBatch.data());
-
-                            count++;
-                        }
-                    } catch (IndexOutOfBoundsException e) {
-                        break;
-                    }
-                }
-            }
-        }
-    }
-
-    @Test
-    public void testEagerIteration() {
-
-        Ops tf = Ops.create();
-
-        List<Operand<?>> tensors = Arrays.asList(
-                tf.constant(testMatrix1),
-                tf.constant(testMatrix2)
-        );
-
-        List<DataType<?>> dataTypes = Arrays.asList(
-                TInt32.DTYPE, TInt32.DTYPE
-        );
-
-        Dataset dataset = Dataset.fromTensorSlices(tf, tensors, dataTypes);
         int count = 0;
-        for (List<Output<?>> outputs : dataset) {
+        while (true) {
+          try {
+            List<Tensor<?>> outputs = session.runner()
+                .fetch(X)
+                .fetch(y)
+                .run();
+
             try (Tensor<TInt32> XBatch =
-                         outputs.get(0).tensor().expect(TInt32.DTYPE);
+                     outputs.get(0).expect(TInt32.DTYPE);
                  Tensor<TInt32> yBatch =
-                         outputs.get(1).tensor().expect(TInt32.DTYPE);) {
+                     outputs.get(1).expect(TInt32.DTYPE);) {
 
-                assertEquals(testMatrix1.get(count), XBatch.data());
-                assertEquals(testMatrix2.get(count), yBatch.data());
+              assertEquals(testMatrix1.get(count), XBatch.data());
+              assertEquals(testMatrix2.get(count), yBatch.data());
 
-                count++;
+              count++;
             }
+          } catch (IndexOutOfBoundsException e) {
+            break;
+          }
         }
+      }
     }
+  }
+
+  @Test
+  public void testEagerIteration() {
+
+    Ops tf = Ops.create();
+
+    List<Operand<?>> tensors = Arrays.asList(
+        tf.constant(testMatrix1),
+        tf.constant(testMatrix2)
+    );
+
+    List<DataType<?>> dataTypes = Arrays.asList(
+        TInt32.DTYPE, TInt32.DTYPE
+    );
+
+    Dataset dataset = Dataset.fromTensorSlices(tf, tensors, dataTypes);
+    int count = 0;
+    for (List<Output<?>> outputs : dataset) {
+      try (Tensor<TInt32> XBatch =
+               outputs.get(0).tensor().expect(TInt32.DTYPE);
+           Tensor<TInt32> yBatch =
+               outputs.get(1).tensor().expect(TInt32.DTYPE);) {
+
+        assertEquals(testMatrix1.get(count), XBatch.data());
+        assertEquals(testMatrix2.get(count), yBatch.data());
+
+        count++;
+      }
+    }
+  }
 }
