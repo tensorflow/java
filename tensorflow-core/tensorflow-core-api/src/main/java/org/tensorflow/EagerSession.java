210be23d48e632a53e03ff9674393f61248f1a11
Dhruv Rajan <dhruv@krishnaprem.com>
2020-04-13 19:36:04 -0700

    add isEager() and isGraph() methods to ExecutionEnvironment

--- tensorflow-core/tensorflow-core-api/src/main/java/org/tensorflow/EagerSession.java
+++ tensorflow-core/tensorflow-core-api/src/main/java/org/tensorflow/EagerSession.java
@@ -176,7 +176,7 @@ public final class EagerSession implements ExecutionEnvironment, AutoCloseable {
     /**
      * Configures the session based on the data found in the provided configuration.
      *
-     * @param value a config protocol buffer
+     * @param config a config protocol buffer
      * @see <a
      *     href="https://github.com/tensorflow/tensorflow/blob/master/tensorflow/core/protobuf/config.proto"/>
      */
@@ -353,6 +353,11 @@ public final class EagerSession implements ExecutionEnvironment, AutoCloseable {
     }
     checkSession();
     return new EagerOperationBuilder(this, type, name);
+  }
+
+  @Override
+  public Environments environmentType() {
+    return Environments.EAGER;
   }
 
   TFE_Context nativeHandle() {
