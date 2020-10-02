c7d04774ea5e6a337f8f79c254deefb46d6270a6
Jim Clarke <JimClarke5@me.com>
2020-10-02 10:45:36 -0400

    added tests for 1D and 3D input

--- tensorflow-framework/src/test/java/org/tensorflow/framework/activations/SoftmaxTest.java
+++ tensorflow-framework/src/test/java/org/tensorflow/framework/activations/SoftmaxTest.java
@@ -18,6 +18,8 @@ import org.junit.jupiter.api.*;
 import org.tensorflow.Operand;
 import org.tensorflow.framework.utils.TestSession;
 import org.tensorflow.op.Ops;
+import org.tensorflow.op.core.ReduceMax;
+import org.tensorflow.op.core.ReduceSum;
 import org.tensorflow.types.TFloat32;
 import org.tensorflow.types.TFloat64;
 import org.tensorflow.types.TInt32;
@@ -100,6 +102,40 @@ public class SoftmaxTest {
     double[][] expected = {
       {1.18405115e-01, 5.89504354e-03, 8.74902034e-01, 7.97807387e-04},
       {1.99088704e-06, 1.19202653e-01, 2.69437261e-07, 8.80795087e-01}
+    };
+    for (TestSession.Mode tfMode : tfModes)
+      try (TestSession session = TestSession.createTestSession(tfMode)) {
+        Ops tf = session.getTF();
+        Softmax<TFloat64> instance = new Softmax<>(tf);
+        Operand<TFloat64> result = instance.call(tf.constant(input));
+        session.evaluate(tf.constant(expected), result);
+      }
+  }
+
+  /** Test of Softmax method, of class Activations. */
+  @Test
+  public void testSoftmax_1D() {
+    double[] input = {1, -2, 3, -4, -5, 6, 7, 8};
+    double[] expected = {
+            6.0352829e-04, 3.0047902e-05, 4.4595040e-03, 4.0665414e-06,
+            1.4959969e-06, 8.9571528e-02, 2.4348068e-01, 6.6184908e-01
+    };
+    for (TestSession.Mode tfMode : tfModes)
+      try (TestSession session = TestSession.createTestSession(tfMode)) {
+        Ops tf = session.getTF();
+        Softmax<TFloat64> instance = new Softmax<>(tf);
+        Operand<TFloat64> result = instance.call(tf.constant(input));
+        session.evaluate(tf.constant(expected), result);
+      }
+  }
+
+  /** Test of Softmax method, of class Activations. */
+  @Test
+  public void testSoftmax_3D() {
+    double[][][] input = {{{1, -2}, {3, -4}}, {{-5, 6}, {-7, 8}}};
+    double[][][] expected = {
+            {{9.5257413e-01, 4.7425874e-02}, {9.9908900e-01, 9.1105123e-04}},
+            {{1.6701422e-05, 9.9998331e-01}, {3.0590220e-07, 9.9999964e-01}}
     };
     for (TestSession.Mode tfMode : tfModes)
       try (TestSession session = TestSession.createTestSession(tfMode)) {
