74ba9b0f384f716989846bd993a831615cadba99
Ryan Nett <rnett@calpoly.edu>
2020-12-07 14:31:57 -0800

    Add javadoc
    Signed-off-by: Ryan Nett <rnett@calpoly.edu>

--- tensorflow-core/tensorflow-core-api/src/main/java/org/tensorflow/op/Index.java
+++ tensorflow-core/tensorflow-core-api/src/main/java/org/tensorflow/op/Index.java
@@ -16,6 +16,22 @@ package org.tensorflow.op;
 
 import org.tensorflow.op.annotation.Endpoint;
 
+/**
+ * Numpy-like indexing.  Supports slices, stride slices, open slices, all, newaxis, and ellipsis.
+ * <p>
+ * Examples:
+ * <pre>{@code
+ * x[1:-1, :, tf.newaxis, ...]
+ * // becomes
+ * stridedSlice(x, Index.slice(1, -1), Index.all(), Index.newAxis(), Index.ellipsis())
+ *
+ *
+ * x[2, 10:, :-10, 2:-2:2]
+ * // becomes
+ * stridedSlice(x, Index.point(2), Index.slice(10, null), Index.slice(null, -10), Index.slice(2, -2, 2))
+ * }</pre>
+ *
+ */
 public abstract class Index {
 
   private final int begin;
@@ -27,7 +43,7 @@ public abstract class Index {
   private final boolean newAxisMask;
   private final boolean shrinkAxisMask;
 
-  public Index(int begin, int end, int stride, boolean beginMask, boolean endMask, boolean ellipsisMask,
+  private Index(int begin, int end, int stride, boolean beginMask, boolean endMask, boolean ellipsisMask,
       boolean newAxisMask, boolean shrinkAxisMask) {
     this.begin = begin;
     this.end = end;
@@ -39,63 +55,91 @@ public abstract class Index {
     this.shrinkAxisMask = shrinkAxisMask;
   }
 
+  /**
+   * @return the beginning index of the slice.
+   */
   public int getBegin() {
     return begin;
   }
 
+  /**
+   * @return the end (exclusive) index of the slice.
+   */
   public int getEnd() {
     return end;
   }
 
+  /**
+   * @return the stride of the slice.
+   */
   public int getStride() {
     return stride;
   }
 
+  /**
+   * @return whether to begin at the beginning.
+   */
   public boolean isBeginMask() {
     return beginMask;
   }
 
+  /**
+   * @return whether to end at the end.
+   */
   public boolean isEndMask() {
     return endMask;
   }
 
+  /**
+   * @return is this index an {@link Ellipses}
+   */
   public boolean isEllipsisMask() {
     return ellipsisMask;
   }
 
+  /**
+   * @return should this index add a new dimension.
+   */
   public boolean isNewAxisMask() {
     return newAxisMask;
   }
 
+  /**
+   * @return should this index shrink its dimension.
+   */
   public boolean isShrinkAxisMask() {
     return shrinkAxisMask;
   }
 
-
+  /**
+   * An index that can be used as the start or end of a slice.
+   */
   public static abstract class Singular extends Index {
 
-    public Singular(int begin, int end, int stride, boolean beginMask, boolean endMask, boolean ellipsisMask,
+    private Singular(int begin, int end, int stride, boolean beginMask, boolean endMask, boolean ellipsisMask,
         boolean newAxisMask, boolean shrinkAxisMask) {
       super(begin, end, stride, beginMask, endMask, ellipsisMask, newAxisMask, shrinkAxisMask);
     }
   }
 
+  /**
+   * An index that gets the entire dimension.
+   */
   public static class All extends Singular {
 
-    public All() {
+    private All() {
       super(0, 0, 1, true, true, false, false, false);
     }
   }
 
-  public static All all(){
-    return new All();
-  }
-
+  /**
+   * An index that gets a single point from its dimension, collapsing it by default.
+   */
   public static class Point extends Singular {
 
     private final int index;
 
-    public Point(int index, boolean keepDim) {
+    private Point(int index, boolean keepDim) {
       super(index, index + 1, 1, false, false, false, false, !keepDim);
       this.index = index;
     }
@@ -105,39 +149,32 @@ public abstract class Index {
     }
   }
 
-  public static Point point(int index){
-    return new Point(index, false);
-  }
-
-  public static Point point(int index, boolean keepDim){
-    return new Point(index, keepDim);
-  }
-
+  /**
+   * An index that adds a new dimension of size 1 where it is used.
+   */
   public static class NewAxis extends Index {
 
-    public NewAxis() {
+    private NewAxis() {
       super(0, 0, 1, false, false, false, true, false);
     }
   }
 
-  public static NewAxis newAxis(){
-    return new NewAxis();
-  }
-
+  /**
+   * An index that expands to get all possible dimensions.
+   */
   public static class Ellipses extends Index {
 
-    public Ellipses() {
+    private Ellipses() {
       super(0, 0, 1, false, false, true, false, false);
     }
   }
 
-  public static Ellipses ellipses(){
-    return new Ellipses();
-  }
-
+  /**
+   * An index to get a slice of its dimension, with an optional stride.
+   */
   public static class Slice extends Index {
 
-    public Slice(Singular start, Singular end, int stride) {
+    private Slice(Singular start, Singular end, int stride) {
       super(start instanceof Point ? ((Point) start).index : 0,
           end instanceof Point ? ((Point) end).index : 0,
           stride,
@@ -153,34 +190,180 @@ public abstract class Index {
     }
   }
 
+  /**
+   * An index that gets the entire dimension.
+   * <p>
+   * Equivalent to Python's {@code :}.
+   */
+  public static All all(){
+    return new All();
+  }
+
+  /**
+   * An index that gets the value at the given position, and collapses the dimension (removing it).
+   * <p>
+   * Equivalent to Python's indexing, and supports negative values in the same way.
+   *
+   * @param index The position to get.
+   */
+  public static Point point(int index){
+    return new Point(index, false);
+  }
+
+
+  /**
+   * An index that gets the value at the given position, and collapses the dimension (removing it) if keepDim is false.
+   *
+   * @param index The position to get.
+   * @param keepDim Whether to keep the dimension as size 1.
+   */
+  public static Point point(int index, boolean keepDim){
+    return new Point(index, keepDim);
+  }
+
+  /**
+   * An index that adds a new dimension of size 1 where it is used.
+   * <p>
+   * Equivalent to Python's {@code np.newaxis}, {@code tf.newaxis} or {@code None}.
+   */
+  public static NewAxis newAxis(){
+    return new NewAxis();
+  }
+
+  /**
+   * An index that expands to get all possible dimensions.
+   * <p>
+   * Equivalent to Python's {@code ...}.
+   */
+  public static Ellipses ellipses(){
+    return new Ellipses();
+  }
+
+  /**
+   * An index to get a slice of its dimension.
+   * Start and end can be null or All to start or end at the beginning or end, respectively.
+   * <p>
+   * Equivalent to Python's {@code :} slicing syntax:
+   * <pre>{@code
+   * :
+   * // becomes
+   * Index.all()
+   * Index.slice(null, null)
+   * Index.slice(Index.all(), Index.all())
+   *
+   * 2:
+   * // becomes
+   * Index.slice(2, null)
+   *
+   * :2
+   * // becomes
+   * Index.slice(null, 2)
+   *
+   * 2:10
+   * // becomes
+   * Index.slice(2, 10)
+   *
+   * :2
+   * // becomes
+   * Index.slice(null, null, 2)
+   *
+   * 2:10:2
+   * //becomes
+   * Index.slice(2, 10, 2)
+   * }</pre>
+   *
+   * @param start Where to start the slice.  Starts at the beginning if null or All.
+   * @param end Where to end the slice (exclusive).  Ends at the end if null or All.
+   * @param stride The stride.
+   */
   public static Slice slice(Singular start, Singular end, int stride){
     return new Slice(start == null ? all() : start, end == null ? all() : end, stride);
   }
 
+
+  /**
+   * An index to get a slice of its dimension.
+   * End can be null or All to end at the end.
+   *
+   * @param start Where to start the slice.
+   * @param end Where to end the slice (exclusive).  Ends at the end if null or All.
+   * @param stride The stride.
+   * @see #slice(Singular, Singular, int)
+   */
   public static Slice slice(int start, Singular end, int stride){
     return slice(point(start), end, stride);
   }
 
+
+  /**
+   * An index to get a slice of its dimension.
+   * Start can be null or All to start at the beginning.
+   *
+   * @param start Where to start the slice.  Starts at the beginning if null or All.
+   * @param end Where to end the slice (exclusive).
+   * @param stride The stride.
+   * @see #slice(Singular, Singular, int)
+   */
   public static Slice slice(Singular start, int end, int stride){
     return slice(start, point(end), stride);
   }
 
+
+  /**
+   * An index to get a slice of its dimension.
+   *
+   * @param start Where to start the slice.
+   * @param end Where to end the slice (exclusive).
+   * @param stride The stride.
+   * @see #slice(Singular, Singular, int)
+   */
   public static Slice slice(int start, int end, int stride){
     return slice(point(start), point(end), stride);
   }
 
+  /**
+   * An index to get a slice of its dimension.
+   * Start and end can be null or All to start or end at the beginning or end, respectively.
+   *
+   * @param start Where to start the slice.  Starts at the beginning if null or All.
+   * @param end Where to end the slice (exclusive).  Ends at the end if null or All.
+   * @see #slice(Singular, Singular, int)
+   */
   public static Slice slice(Singular start, Singular end){
     return slice(start, end, 1);
   }
 
+  /**
+   * An index to get a slice of its dimension.
+   * End can be null or All to end at the end.
+   *
+   * @param start Where to start the slice.
+   * @param end Where to end the slice (exclusive).  Ends at the end if null or All.
+   * @see #slice(Singular, Singular, int)
+   */
   public static Slice slice(int start, Singular end){
     return slice(start, end, 1);
   }
 
+  /**
+   * An index to get a slice of its dimension.
+   * Start can be null or All to start at the beginning.
+   *
+   * @param start Where to start the slice.  Starts at the beginning if null or All.
+   * @param end Where to end the slice (exclusive).
+   * @see #slice(Singular, Singular, int)
+   */
   public static Slice slice(Singular start, int end){
     return slice(start, end, 1);
   }
 
+  /**
+   * An index to get a slice of its dimension.
+   *
+   * @param start Where to start the slice.
+   * @param end Where to end the slice (exclusive).
+   * @see #slice(Singular, Singular, int)
+   */
   public static Slice slice(int start, int end){
     return slice(start, end, 1);
   }
--- tensorflow-core/tensorflow-core-api/src/main/java/org/tensorflow/op/core/Indexing.java
+++ tensorflow-core/tensorflow-core-api/src/main/java/org/tensorflow/op/core/Indexing.java
@@ -16,11 +16,17 @@ package org.tensorflow.op.core;
 
 import org.tensorflow.Operand;
 import org.tensorflow.op.Index;
+import org.tensorflow.op.Index.Singular;
 import org.tensorflow.op.Scope;
 import org.tensorflow.op.annotation.Endpoint;
 import org.tensorflow.op.annotation.Operator;
 import org.tensorflow.types.family.TType;
 
+/**
+ * Helper endpoint methods for Python like indexing.
+ *
+ * @see Index
+ */
 @Operator
 public class Indexing {
 
@@ -77,6 +83,8 @@ public class Indexing {
       }
 
       if (idx.isEllipsisMask()) {
+        if(ellipsisMask != 0)
+          throw new IllegalArgumentException("Can not have two ellipsis in a slice");
         ellipsisMask |= 1L << i;
       }
 
@@ -91,7 +99,56 @@ public class Indexing {
 
     return new StridedSliceArgs(begin, end, strides, beginMask, endMask, ellipsisMask, newAxisMask, shrinkAxisMask);
   }
-
+  /**
+   * Return a strided slice from `input`.
+   *  <p>
+   *  The goal of this op is to produce a new tensor with a subset of
+   *  the elements from the `n` dimensional `input` tensor. The subset is chosen using
+   *  a sequence of `m` sparse range specifications encoded into the arguments
+   *  of this function. Note, in some cases
+   *  `m` could be equal to `n`, but this need not be the case. Each
+   *  range specification entry can be one of the following:
+   *  <p>
+   *  - An ellipsis (...) using {@link Index#ellipses()}. Ellipses are used to imply zero or more
+   *    dimensions of full-dimension selection and are produced using
+   *    `ellipsis_mask`. For example, `foo[...]` is the identity slice.
+   *  <p>
+   *  - A new axis using {@link Index#newAxis()}. This is used to insert a new shape=1 dimension and is
+   *    produced using `new_axis_mask`. For example, `foo[:, ...]` where
+   *    `foo` is shape `(3, 4)` produces a `(1, 3, 4)` tensor.
+   *  <p>
+   *  - A range `begin:end:stride` using {@link Index#slice(Singular, Singular, int) Index.slice()} or {@link Index#all()}. This is used to specify how much to choose from
+   *    a given dimension. `stride` can be any integer but 0.  `begin` is an integer
+   *    which represents the index of the first value to select while `end` represents
+   *    the index of the last value to select. The number of values selected in each
+   *    dimension is `end - begin` if `stride > 0` and `begin - end` if `stride < 0`.
+   *    `begin` and `end` can be negative where `-1` is the last element, `-2` is
+   *    the second to last. `begin_mask` controls whether to replace the explicitly
+   *    given `begin` with an implicit effective value of `0` if `stride > 0` and
+   *    `-1` if `stride < 0`. `end_mask` is analogous but produces the number
+   *    required to create the largest open interval. For example, given a shape
+   *    `(3,)` tensor `foo[:]`, the effective `begin` and `end` are `0` and `3`. Do
+   *    not assume this is equivalent to `foo[0:-1]` which has an effective `begin`
+   *    and `end` of `0` and `2`. Another example is `foo[-2::-1]` which reverses the
+   *    first dimension of a tensor while dropping the last two (in the original
+   *    order elements). For example `foo = [1,2,3,4]; foo[-2::-1]` is `[4,3]`.
+   *  <p>
+   *  - A single index using {@link Index#point(int)}. This is used to keep only elements that have a given
+   *    index. For example (`foo[2, :]` on a shape `(5,6)` tensor produces a
+   *    shape `(6,)` tensor. This is encoded in `begin` and `end` and
+   *    `shrink_axis_mask`.
+   *  <p>
+   *
+   *  <i>Requirements</i>:
+   *    `0 != strides[i] for i in [0, m)`
+   *    Only one ellipsis.
+   *
+   * @param scope current scope
+   * @param <T> data type for {@code output()} output
+   * @param input
+   * @param indices The indices to slice.  See {@link Index}.
+   * @return a new instance of StridedSlice
+   */
   @Endpoint(name = "stridedSlice")
   public static <T extends TType> StridedSlice<T> stridedSlice(Scope scope, Operand<T> input, Index... indices) {
     StridedSliceArgs args = mergeIndexes(indices);
@@ -109,6 +166,24 @@ public class Indexing {
     );
   }
 
+  /**
+   * Assign `value` to the sliced l-value reference of `ref`.
+   *  <p>
+   *  The values of `value` are assigned to the positions in the variable
+   *  `ref` that are selected by the slice parameters. The slice parameters
+   *  `begin`, `end`, `strides`, etc. work exactly as in `StridedSlice`.
+   *  <p>
+   *  NOTE this op currently does not support broadcasting and so `value`'s
+   *  shape must be exactly the shape produced by the slice of `ref`.
+   *
+   * @param <T> data type for {@code outputRef()} output
+   * @param scope current scope
+   * @param ref the tensor to assign to.
+   * @param value the value to assign.
+   * @param indices The indices to slice.  See {@link Index}.
+   * @return a new instance of StridedSliceAssign
+   * @see #stridedSlice(Scope, Operand, Index...)
+   */
   @Endpoint(name = "stridedSliceAssign")
   public static <T extends TType> StridedSliceAssign<T> stridedSliceAssign(Scope scope, Operand<T> ref, Operand<T> value, Index... indices) {
     StridedSliceArgs args = mergeIndexes(indices);
