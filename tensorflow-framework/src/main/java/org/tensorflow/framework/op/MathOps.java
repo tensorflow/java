4d3f17cf4cff04dee66f2e00756d911eaf12e2bd
Jim Clarke <JimClarke5@me.com>
2021-03-27 15:36:41 -0400

    Move l2Normalize to MathOps

---
+++ tensorflow-framework/src/main/java/org/tensorflow/framework/op/MathOps.java
@@ -0,0 +1,67 @@
+/* Copyright 2020 The TensorFlow Authors. All Rights Reserved.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+=======================================================================*/
+package org.tensorflow.framework.op;
+
+import org.tensorflow.Operand;
+import org.tensorflow.op.Ops;
+import org.tensorflow.op.Scope;
+import org.tensorflow.op.core.Constant;
+import org.tensorflow.op.core.ReduceSum;
+import org.tensorflow.op.dtypes.Cast;
+import org.tensorflow.op.math.Maximum;
+import org.tensorflow.op.math.Mul;
+import org.tensorflow.op.math.Rsqrt;
+import org.tensorflow.op.math.Square;
+import org.tensorflow.types.family.TNumber;
+
+import static org.tensorflow.framework.utils.CastHelper.cast;
+
+public class MathOps {
+    private final Scope scope;
+
+    private final FrameworkOps frameworkOps;
+
+    /**
+     * Creates Framework {@code nn} Operations
+     *
+     * @param frameworkOps the TensorFLow framework Ops
+     */
+    MathOps(FrameworkOps frameworkOps) {
+        this.scope = frameworkOps.scope();
+        this.frameworkOps = frameworkOps;
+    }
+
+    /**
+     * Normalizes along dimension axis using an L2 norm.
+     *
+     * @param x the input
+     * @param axis Dimension along which to normalize.
+     * @param <T> the data type for the input and the result
+     * @return the normalized values based on L2 norm
+     */
+    public  <T extends TNumber> Operand<T> l2Normalize(Operand<T> x, int[] axis) {
+        Operand<T> squareSum =
+                ReduceSum.create(scope,
+                        Square.create(scope, x),
+                        Constant.vectorOf(scope, axis),
+                        ReduceSum.keepDims(Boolean.TRUE));
+        Operand<T> invNorm =
+                Rsqrt.create(scope,
+                        Maximum.create(scope, squareSum,
+                                Cast.create(scope,
+                                        Constant.scalarOf(scope, 1e-12F), x.type())));
+        return Mul.create(scope, x, invNorm);
+    }
+}
