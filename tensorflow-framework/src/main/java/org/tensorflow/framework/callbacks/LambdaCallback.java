9efff83a02343e813d87a3b2bfa122b8d56d3a5c
Jim Clarke <JimClarke5@me.com>
2021-05-06 17:26:41 -0400

    Added missing methods in Lambda Callback

--- tensorflow-framework/src/main/java/org/tensorflow/framework/callbacks/LambdaCallback.java
+++ tensorflow-framework/src/main/java/org/tensorflow/framework/callbacks/LambdaCallback.java
@@ -61,6 +61,30 @@ public class LambdaCallback extends Callback {
   /** called at the end of model training. expect one positional argument: `logs` */
   private Consumer<Map<String, Number>> onTrainEnd;
 
+  /** Called at the beginning of every batch. expect two positional arguments: `batch`, `logs` */
+  private BiConsumer<Integer, Map<String, Number>> onTestBatchBegin;
+
+  /** called at the end of every batch. expect two positional arguments: `batch`, `logs` */
+  private BiConsumer<Integer, Map<String, Number>> onTestBatchEnd;
+
+  /** called at the beginning of model training. expect one positional argument: `logs` */
+  private Consumer<Map<String, Number>> onTestBegin;
+
+  /** called at the end of model training. expect one positional argument: `logs` */
+  private Consumer<Map<String, Number>> onTestEnd;
+
+  /** Called at the beginning of every batch. expect two positional arguments: `batch`, `logs` */
+  private BiConsumer<Integer, Map<String, Number>> onPredictBatchBegin;
+
+  /** called at the end of every batch. expect two positional arguments: `batch`, `logs` */
+  private BiConsumer<Integer, Map<String, Number>> onPredictBatchEnd;
+
+  /** called at the beginning of model training. expect one positional argument: `logs` */
+  private Consumer<Map<String, Number>> onPredictBegin;
+
+  /** called at the end of model training. expect one positional argument: `logs` */
+  private Consumer<Map<String, Number>> onPredictEnd;
+
   /** Creates a LambdaCallbacks callback */
   public LambdaCallback() {
     super();
@@ -122,6 +146,72 @@ public class LambdaCallback extends Callback {
       this.onTrainEnd.accept(logs);
     }
   }
+
+  /** {@inheritDoc} */
+  @Override
+  public void onTestBatchBegin(int batch, Map<String, Number> logs) {
+    if (this.onTestBatchBegin != null) {
+      this.onTestBatchBegin.accept(batch, logs);
+    }
+  }
+
+  /** {@inheritDoc} */
+  @Override
+  public void onTestBatchEnd(int batch, Map<String, Number> logs) {
+    if (this.onTestBatchEnd != null) {
+      this.onTestBatchEnd.accept(batch, logs);
+    }
+  }
+
+  /** {@inheritDoc} */
+  @Override
+  public void onTestBegin(Map<String, Number> logs) {
+    if (this.onTestBegin != null) {
+      this.onTestBegin.accept(logs);
+    }
+  }
+
+  /** {@inheritDoc} */
+  @Override
+  public void onTestEnd(Map<String, Number> logs) {
+    if (this.onTestEnd != null) {
+      this.onTestEnd.accept(logs);
+    }
+  }
+
+
+  /** {@inheritDoc} */
+  @Override
+  public void onPredictBatchBegin(int batch, Map<String, Number> logs) {
+    if (this.onPredictBatchBegin != null) {
+      this.onPredictBatchBegin.accept(batch, logs);
+    }
+  }
+
+  /** {@inheritDoc} */
+  @Override
+  public void onPredictBatchEnd(int batch, Map<String, Number> logs) {
+    if (this.onPredictBatchEnd != null) {
+      this.onPredictBatchEnd.accept(batch, logs);
+    }
+  }
+
+  /** {@inheritDoc} */
+  @Override
+  public void onPredictBegin(Map<String, Number> logs) {
+    if (this.onPredictBegin != null) {
+      this.onPredictBegin.accept(logs);
+    }
+  }
+
+  /** {@inheritDoc} */
+  @Override
+  public void onPredictEnd(Map<String, Number> logs) {
+    if (this.onPredictEnd != null) {
+      this.onPredictEnd.accept(logs);
+    }
+  }
+
 
   /**
    * Gets the onEpochBegin lambda function
@@ -230,4 +320,151 @@ public class LambdaCallback extends Callback {
   public void setOnTrainEnd(Consumer<Map<String, Number>> onTrainEnd) {
     this.onTrainEnd = onTrainEnd;
   }
+
+  /**
+   * Gets the onTestBatchBegin lambda function
+   *
+   * @return the onTestBatchBegin lambda function
+   */
+  public BiConsumer<Integer, Map<String, Number>> getOnTestBatchBegin() {
+    return onTestBatchBegin;
+  }
+
+  /**
+   * Sets the onTestBatchBegin lambda function
+   *
+   * @param onTestBatchBegin the lambda function
+   */
+  public void setOnTestBatchBegin(BiConsumer<Integer, Map<String, Number>> onTestBatchBegin) {
+    this.onTestBatchBegin = onTestBatchBegin;
+  }
+
+  /**
+   * Gets the onTestBatchEnd lambda function
+   *
+   * @return the onTestBatchEnd lambda function
+   */
+  public BiConsumer<Integer, Map<String, Number>> getOnTestBatchEnd() {
+    return onTestBatchEnd;
+  }
+
+  /**
+   * Sets the onTestBatchEnd lambda function
+   *
+   * @param onTestBatchEnd the onTestBatchEnd lambda function
+   */
+  public void setOnTestBatchEnd(BiConsumer<Integer, Map<String, Number>> onTestBatchEnd) {
+    this.onTestBatchEnd = onTestBatchEnd;
+  }
+
+  /**
+   * Gets the onTestBegin lambda function
+   *
+   * @return the onTestBegin lambda function
+   */
+  public Consumer<Map<String, Number>> getOnTestBegin() {
+    return onTestBegin;
+  }
+
+  /**
+   * Sets the onTestBegin lambda function
+   *
+   * @param onTestBegin the onTestBegin lambda function
+   */
+  public void setOnTestBegin(Consumer<Map<String, Number>> onTestBegin) {
+    this.onTestBegin = onTestBegin;
+  }
+
+  /**
+   * Gets the onTestBegin lambda function
+   *
+   * @return the onTestEnd lambda function
+   */
+  public Consumer<Map<String, Number>> onTestEnd() {
+    return onTestEnd;
+  }
+
+  /**
+   * Sets the onTestEnd lambda function
+   *
+   * @param onTestEnd the onTestBegin lambda function
+   */
+  public void setOnTestEnd(Consumer<Map<String, Number>> onTestEnd) {
+    this.onTestEnd = onTestEnd;
+  }
+
+
+  /**
+   * Gets the onPredictBatchBegin lambda function
+   *
+   * @return the onPredictBatchBegin lambda function
+   */
+  public BiConsumer<Integer, Map<String, Number>> getOnPredictBatchBegin() {
+    return onPredictBatchBegin;
+  }
+
+  /**
+   * Sets the onPredictBatchBegin lambda function
+   *
+   * @param onPredictBatchBegin the lambda function
+   */
+  public void setOnPredictBatchBegin(BiConsumer<Integer, Map<String, Number>> onPredictBatchBegin) {
+    this.onPredictBatchBegin = onPredictBatchBegin;
+  }
+
+  /**
+   * Gets the onPredictBatchEnd lambda function
+   *
+   * @return the onPredictBatchEnd lambda function
+   */
+  public BiConsumer<Integer, Map<String, Number>> getOnPredictBatchEnd() {
+    return onPredictBatchEnd;
+  }
+
+  /**
+   * Sets the onPredictBatchEnd lambda function
+   *
+   * @param onPredictBatchEnd the onPredictBatchEnd lambda function
+   */
+  public void setOnPredictBatchEnd(BiConsumer<Integer, Map<String, Number>> onPredictBatchEnd) {
+    this.onPredictBatchEnd = onPredictBatchEnd;
+  }
+
+  /**
+   * Gets the onPredictBegin lambda function
+   *
+   * @return the onPredictBegin lambda function
+   */
+  public Consumer<Map<String, Number>> getOnPredictBegin() {
+    return onPredictBegin;
+  }
+
+  /**
+   * Sets the onPredictBegin lambda function
+   *
+   * @param onPredictBegin the onPredictBegin lambda function
+   */
+  public void setOnPredictBegin(Consumer<Map<String, Number>> onPredictBegin) {
+    this.onPredictBegin = onPredictBegin;
+  }
+
+  /**
+   * Gets the onPredictEnd lambda function
+   *
+   * @return the onPredictEnd lambda function
+   */
+  public Consumer<Map<String, Number>> getOnPredictEnd() {
+    return onPredictEnd;
+  }
+
+  /**
+   * Sets the onPredictEnd lambda function
+   *
+   * @param onPredictEnd the onPredictEnd lambda function
+   */
+  public void setOnPredictEnd(Consumer<Map<String, Number>> onPredictEnd) {
+    this.onPredictEnd = onPredictEnd;
+  }
+
+
 }
--- tensorflow-framework/src/test/java/org/tensorflow/framework/callbacks/LambdaCallbackTest.java
+++ tensorflow-framework/src/test/java/org/tensorflow/framework/callbacks/LambdaCallbackTest.java
@@ -47,17 +47,260 @@ class LambdaCallbackTest {
   }
 
   @Test
-  void onEpochEnd() {}
+  void onEpochEnd() {
+    LambdaCallback instance = new LambdaCallback();
+    final AtomicBoolean called = new AtomicBoolean(false);
+    int expectedEpoch = 101;
+    Map<String, Number> exceptedLog = new HashMap<>();
+    exceptedLog.put("acc", 0.98);
+    instance.setOnEpochEnd(
+        (epoch, log) -> {
+          assertEquals(expectedEpoch, epoch);
+          assertEquals(exceptedLog, log);
+          called.set(true);
+        });
+
+    Map<String, Number> epochLog = new HashMap<>();
+    epochLog.put("acc", 0.98);
+    instance.onEpochEnd(101, epochLog);
+
+    assertTrue(called.get());
+  }
 
   @Test
-  void onTrainBatchBegin() {}
+  void onTrainBatchBegin() {
+    LambdaCallback instance = new LambdaCallback();
+    final AtomicBoolean called = new AtomicBoolean(false);
+    int expectedBatch = 101;
+    Map<String, Number> exceptedLog = new HashMap<>();
+    exceptedLog.put("acc", 0.98);
+    instance.setOnTrainBatchBegin(
+        (batch, log) -> {
+          assertEquals(expectedBatch, batch);
+          assertEquals(exceptedLog, log);
+          called.set(true);
+        });
+
+    Map<String, Number> epochLog = new HashMap<>();
+    epochLog.put("acc", 0.98);
+    instance.onTrainBatchBegin(101, epochLog);
+
+    assertTrue(called.get());
+  }
 
   @Test
-  void onTrainBatchEnd() {}
+  void onTrainBatchEnd() {
+    LambdaCallback instance = new LambdaCallback();
+    final AtomicBoolean called = new AtomicBoolean(false);
+    int expectedBatch = 101;
+    Map<String, Number> exceptedLog = new HashMap<>();
+    exceptedLog.put("acc", 0.98);
+    instance.setOnTrainBatchEnd(
+        (batch, log) -> {
+          assertEquals(expectedBatch, batch);
+          assertEquals(exceptedLog, log);
+          called.set(true);
+        });
+
+    Map<String, Number> epochLog = new HashMap<>();
+    epochLog.put("acc", 0.98);
+    instance.onTrainBatchEnd(101, epochLog);
+
+    assertTrue(called.get());
+  }
 
   @Test
-  void onTrainBegin() {}
+  void onTrainBegin() {
+    LambdaCallback instance = new LambdaCallback();
+    final AtomicBoolean called = new AtomicBoolean(false);
+    Map<String, Number> exceptedLog = new HashMap<>();
+    exceptedLog.put("acc", 0.98);
+    instance.setOnTrainBegin(
+        logs -> {
+          assertEquals(exceptedLog, logs);
+          called.set(true);
+        });
+
+    Map<String, Number> log = new HashMap<>();
+    log.put("acc", 0.98);
+    instance.onTrainBegin(log);
+
+    assertTrue(called.get());
+  }
 
   @Test
-  void onTrainEnd() {}
+  void onTrainEnd() {
+    LambdaCallback instance = new LambdaCallback();
+    final AtomicBoolean called = new AtomicBoolean(false);
+    Map<String, Number> expectedLog = new HashMap<>();
+    expectedLog.put("acc", 0.98);
+    instance.setOnTrainEnd(
+        logs -> {
+          assertEquals(expectedLog, logs);
+          called.set(true);
+        });
+    Map<String, Number> log = new HashMap<>();
+    log.put("acc", 0.98);
+    instance.onTrainEnd(log);
+
+    assertTrue(called.get());
+  }
+
+  @Test
+  void onTestBatchBegin() {
+    LambdaCallback instance = new LambdaCallback();
+    final AtomicBoolean called = new AtomicBoolean(false);
+    int expectedBatch = 101;
+    Map<String, Number> exceptedLog = new HashMap<>();
+    exceptedLog.put("acc", 0.98);
+    instance.setOnTestBatchBegin(
+        (batch, log) -> {
+          assertEquals(expectedBatch, batch);
+          assertEquals(exceptedLog, log);
+          called.set(true);
+        });
+
+    Map<String, Number> epochLog = new HashMap<>();
+    epochLog.put("acc", 0.98);
+    instance.onTestBatchBegin(101, epochLog);
+
+    assertTrue(called.get());
+  }
+
+  @Test
+  void onTestBatchEnd() {
+    LambdaCallback instance = new LambdaCallback();
+    final AtomicBoolean called = new AtomicBoolean(false);
+    int expectedBatch = 101;
+    Map<String, Number> exceptedLog = new HashMap<>();
+    exceptedLog.put("acc", 0.98);
+    instance.setOnTestBatchEnd(
+        (batch, log) -> {
+          assertEquals(expectedBatch, batch);
+          assertEquals(exceptedLog, log);
+          called.set(true);
+        });
+
+    Map<String, Number> epochLog = new HashMap<>();
+    epochLog.put("acc", 0.98);
+    instance.onTestBatchEnd(101, epochLog);
+
+    assertTrue(called.get());
+  }
+
+  @Test
+  void onTestBegin() {
+    LambdaCallback instance = new LambdaCallback();
+    final AtomicBoolean called = new AtomicBoolean(false);
+    Map<String, Number> exceptedLog = new HashMap<>();
+    exceptedLog.put("acc", 0.98);
+    instance.setOnTestBegin(
+        logs -> {
+          assertEquals(exceptedLog, logs);
+          called.set(true);
+        });
+
+    Map<String, Number> log = new HashMap<>();
+    log.put("acc", 0.98);
+    instance.onTestBegin(log);
+
+    assertTrue(called.get());
+  }
+
+  @Test
+  void onTestEnd() {
+    LambdaCallback instance = new LambdaCallback();
+    final AtomicBoolean called = new AtomicBoolean(false);
+    Map<String, Number> expectedLog = new HashMap<>();
+    expectedLog.put("acc", 0.98);
+    instance.setOnTestEnd(
+        logs -> {
+          assertEquals(expectedLog, logs);
+          called.set(true);
+        });
+    Map<String, Number> log = new HashMap<>();
+    log.put("acc", 0.98);
+    instance.onTestEnd(log);
+
+    assertTrue(called.get());
+  }
+
+  @Test
+  void onPredictBatchBegin() {
+    LambdaCallback instance = new LambdaCallback();
+    final AtomicBoolean called = new AtomicBoolean(false);
+    int expectedBatch = 101;
+    Map<String, Number> exceptedLog = new HashMap<>();
+    exceptedLog.put("acc", 0.98);
+    instance.setOnPredictBatchBegin(
+        (batch, log) -> {
+          assertEquals(expectedBatch, batch);
+          assertEquals(exceptedLog, log);
+          called.set(true);
+        });
+
+    Map<String, Number> epochLog = new HashMap<>();
+    epochLog.put("acc", 0.98);
+    instance.onPredictBatchBegin(101, epochLog);
+
+    assertTrue(called.get());
+  }
+
+  @Test
+  void onPredictBatchEnd() {
+    LambdaCallback instance = new LambdaCallback();
+    final AtomicBoolean called = new AtomicBoolean(false);
+    int expectedBatch = 101;
+    Map<String, Number> exceptedLog = new HashMap<>();
+    exceptedLog.put("acc", 0.98);
+    instance.setOnPredictBatchEnd(
+        (batch, log) -> {
+          assertEquals(expectedBatch, batch);
+          assertEquals(exceptedLog, log);
+          called.set(true);
+        });
+
+    Map<String, Number> epochLog = new HashMap<>();
+    epochLog.put("acc", 0.98);
+    instance.onPredictBatchEnd(101, epochLog);
+
+    assertTrue(called.get());
+  }
+
+  @Test
+  void onPredictBegin() {
+    LambdaCallback instance = new LambdaCallback();
+    final AtomicBoolean called = new AtomicBoolean(false);
+    Map<String, Number> exceptedLog = new HashMap<>();
+    exceptedLog.put("acc", 0.98);
+    instance.setOnPredictBegin(
+        logs -> {
+          assertEquals(exceptedLog, logs);
+          called.set(true);
+        });
+
+    Map<String, Number> log = new HashMap<>();
+    log.put("acc", 0.98);
+    instance.onPredictBegin(log);
+
+    assertTrue(called.get());
+  }
+
+  @Test
+  void onPredictEnd() {
+    LambdaCallback instance = new LambdaCallback();
+    final AtomicBoolean called = new AtomicBoolean(false);
+    Map<String, Number> expectedLog = new HashMap<>();
+    expectedLog.put("acc", 0.98);
+    instance.setOnPredictEnd(
+        logs -> {
+          assertEquals(expectedLog, logs);
+          called.set(true);
+        });
+    Map<String, Number> log = new HashMap<>();
+    log.put("acc", 0.98);
+    instance.onPredictEnd(log);
+
+    assertTrue(called.get());
+  }
 }
