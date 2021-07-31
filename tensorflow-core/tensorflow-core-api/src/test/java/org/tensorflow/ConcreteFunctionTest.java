429f66bd71be16f66b492cbf7c285d761ab82b9d
Ryan Nett <JNett96@gmail.com>
2021-07-30 18:56:30 -0700

    ConcreteFunction fix and performance improvements
    Signed-off-by: Ryan Nett <JNett96@gmail.com>

--- tensorflow-core/tensorflow-core-api/src/test/java/org/tensorflow/ConcreteFunctionTest.java
+++ tensorflow-core/tensorflow-core-api/src/test/java/org/tensorflow/ConcreteFunctionTest.java
@@ -1,24 +1,26 @@
 /* Copyright 2020-2021 The TensorFlow Authors. All Rights Reserved.
 
- Licensed under the Apache License, Version 2.0 (the "License");
- you may not use this file except in compliance with the License.
- You may obtain a copy of the License at
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
 
-     http://www.apache.org/licenses/LICENSE-2.0
+    http://www.apache.org/licenses/LICENSE-2.0
 
- Unless required by applicable law or agreed to in writing, software
- distributed under the License is distributed on an "AS IS" BASIS,
- WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- See the License for the specific language governing permissions and
- limitations under the License.
- =======================================================================
- */
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+=======================================================================
+*/
 package org.tensorflow;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
 import org.junit.jupiter.api.Test;
 import org.tensorflow.op.Ops;
 import org.tensorflow.op.core.Init;
@@ -27,6 +29,7 @@ import org.tensorflow.op.math.Add;
 import org.tensorflow.op.math.Sub;
 import org.tensorflow.proto.framework.DataType;
 import org.tensorflow.types.TFloat32;
+import org.tensorflow.types.TInt32;
 
 public class ConcreteFunctionTest {
 
@@ -142,6 +145,28 @@ public class ConcreteFunctionTest {
         assertEquals(13f, t.getFloat());
       }
     }
+  }
+
+  @Test
+  public void testFunctionWithTwoOutputs() {
+    ConcreteFunction cf =
+        ConcreteFunction.create(
+            tf -> {
+              Placeholder<TInt32> x = tf.placeholder(TInt32.class);
+              Operand<TInt32> dblX = tf.math.add(x, x);
+              Operand<TInt32> tripX = tf.math.add(x, dblX);
+              return Signature.builder()
+                  .input("x", x)
+                  .output("dbl", dblX)
+                  .output("trpl", tripX)
+                  .build();
+            });
+
+    Map<String, Tensor> inputs = new HashMap<>();
+    inputs.put("x", TInt32.scalarOf(2));
+    Map<String, Tensor> outputs = cf.call(inputs);
+    assertEquals(4, ((TInt32) outputs.get("dbl")).getInt());
+    assertEquals(6, ((TInt32) outputs.get("trpl")).getInt());
   }
 
   private static Signature square(Ops tf) {
