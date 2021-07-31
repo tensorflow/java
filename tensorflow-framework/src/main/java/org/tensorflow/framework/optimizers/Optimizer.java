07143aea725f735ad2d9b1cdcab32774dbc78472
Ryan Nett <JNett96@gmail.com>
2021-07-30 19:09:18 -0700

    Remove wildcard import
    Signed-off-by: Ryan Nett <JNett96@gmail.com>

--- tensorflow-framework/src/main/java/org/tensorflow/framework/optimizers/Optimizer.java
+++ tensorflow-framework/src/main/java/org/tensorflow/framework/optimizers/Optimizer.java
@@ -15,7 +15,11 @@
  */
 package org.tensorflow.framework.optimizers;
 
-import java.util.*;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
 import java.util.stream.Collectors;
 import org.tensorflow.Graph;
 import org.tensorflow.Operand;
