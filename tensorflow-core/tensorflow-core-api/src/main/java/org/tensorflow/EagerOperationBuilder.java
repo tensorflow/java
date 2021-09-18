0895e48694b006921b91d20ae51d1af8b9604154
Ryan Nett <JNett96@gmail.com>
2021-09-18 16:14:53 -0700

    Rebase fixes
    Signed-off-by: Ryan Nett <JNett96@gmail.com>

--- tensorflow-core/tensorflow-core-api/src/main/java/org/tensorflow/EagerOperationBuilder.java
+++ tensorflow-core/tensorflow-core-api/src/main/java/org/tensorflow/EagerOperationBuilder.java
@@ -54,8 +54,8 @@ import org.tensorflow.internal.c_api.TFE_TensorHandle;
 import org.tensorflow.internal.c_api.TF_Status;
 import org.tensorflow.internal.c_api.TF_Tensor;
 import org.tensorflow.ndarray.Shape;
-import org.tensorflow.proto.framework.AttrValue;
 import org.tensorflow.op.Scope;
+import org.tensorflow.proto.framework.AttrValue;
 import org.tensorflow.proto.framework.DataType;
 
 /**
