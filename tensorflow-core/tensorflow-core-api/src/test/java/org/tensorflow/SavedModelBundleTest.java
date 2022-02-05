ad9172c68b556b3aad206cde72cd12caf6c2fdc5
Adam Pocock <adam.pocock@oracle.com>
2022-02-05 15:47:38 -0500

    Missed a bit in the refactor.

--- tensorflow-core/tensorflow-core-api/src/test/java/org/tensorflow/SavedModelBundleTest.java
+++ tensorflow-core/tensorflow-core-api/src/test/java/org/tensorflow/SavedModelBundleTest.java
@@ -215,7 +215,7 @@ public class SavedModelBundleTest {
         // Now call the same function directly from the model
         try (TFloat32 zTensor =
             (TFloat32)
-                savedModel.call(Collections.singletonMap("input", xTensor)).get("reducedSum")) {
+                savedModel.call(Collections.singletonMap("input", xTensor)).get("reducedSum").get()) {
           assertEquals(reducedSum, zTensor.getFloat(), EPSILON);
         }
       }
