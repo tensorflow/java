50a9a367cd0c87fdb69322cd56e624fa267d75f9
Karl Lessard <karl.lessard@gmail.com>
2022-03-03 09:06:41 -0500

    Fix build on JDK17 and spotless checks

--- tensorflow-core/tensorflow-core-api/src/main/java/module-info.java
+++ tensorflow-core/tensorflow-core-api/src/main/java/module-info.java
@@ -1,19 +1,19 @@
 /*
- Copyright 2022 The TensorFlow Authors. All Rights Reserved.
+Copyright 2022 The TensorFlow Authors. All Rights Reserved.
 
- Licensed under the Apache License, Version 2.0 (the "License");
- you may not use this file except in compliance with the License.
- You may obtain a copy of the License at
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
 
-     http://www.apache.org/licenses/LICENSE-2.0
+    http://www.apache.org/licenses/LICENSE-2.0
 
- Unless required by applicable law or agreed to in writing, software
- distributed under the License is distributed on an "AS IS" BASIS,
- WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- See the License for the specific language governing permissions and
- limitations under the License.
- =======================================================================
- */
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+=======================================================================
+*/
 module org.tensorflow {
   requires transitive org.tensorflow.ndarray;
   requires org.bytedeco.javacpp;
@@ -25,7 +25,6 @@ module org.tensorflow {
   exports org.tensorflow.types;
   exports org.tensorflow.types.annotation;
   exports org.tensorflow.types.family;
-
   exports org.tensorflow.op;
   exports org.tensorflow.op.annotation;
   exports org.tensorflow.op.core;
@@ -56,7 +55,6 @@ module org.tensorflow {
   exports org.tensorflow.op.tpu;
   exports org.tensorflow.op.train;
   exports org.tensorflow.op.xla;
-
   exports org.tensorflow.proto.data;
   exports org.tensorflow.proto.data.experimental;
   exports org.tensorflow.proto.data.model;
