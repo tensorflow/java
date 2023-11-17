50421ecd9e10ceb37ba350531079cb2eda9622be
Karl Lessard <karl.lessard@gmail.com>
2023-11-17 14:06:42 -0500

    Add missing ops and move more files around

--- tensorflow-core/tensorflow-core-native/src/main/java/module-info.java
+++ tensorflow-core/tensorflow-core-native/src/main/java/module-info.java
@@ -14,7 +14,7 @@ See the License for the specific language governing permissions and
 limitations under the License.
 =======================================================================
 */
-module tensorflow.tf_native {
+module tensorflow.runtime {
   requires transitive org.bytedeco.javacpp;
   requires transitive com.google.protobuf;
   requires java.logging;
