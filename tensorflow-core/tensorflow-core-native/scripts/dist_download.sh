9e3f293d15508083e355a03294a95aeb4c0d6e47
Karl Lessard <karl.lessard@gmail.com>
2023-11-24 16:29:36 -0500

    Move URLs to download script + new CI

---
+++ tensorflow-core/tensorflow-core-native/scripts/dist_download.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+set -eu
+
+DOWNLOAD_FOLDER="$1"
+DOWNLOADED_FILE="tensorflow.zip" # TODO extract name from wheel url, change .whl to .zip
+
+case ${PLATFORM:-} in
+  'linux-x86_64')
+    DOWNLOAD_URL='https://files.pythonhosted.org/packages/57/bf/03debeeaeca996543478db89c8729a40dcb97a6be811804f4a08c8e1c75a/tensorflow_cpu-2.14.0-cp311-cp311-manylinux_2_17_x86_64.manylinux2014_x86_64.whl'
+    ;;
+  'linux-x86_64-gpu')
+    DOWNLOAD_URL='https://files.pythonhosted.org/packages/09/63/25e76075081ea98ec48f23929cefee58be0b42212e38074a9ec5c19e838c/tensorflow-2.14.0-cp311-cp311-manylinux_2_17_x86_64.manylinux2014_x86_64.whl'
+    ;;
+  'macosx-x86_64')
+    DOWNLOAD_URL='https://files.pythonhosted.org/packages/ad/e2/ff29a5e514ec25470b59e5d0134f08f75bd28d6ec6941a22bea6b78cb2fb/tensorflow_cpu-2.14.0-cp311-cp311-macosx_10_15_x86_64.whl'
+    ;;
+  'macosx-arm64')
+    DOWNLOAD_URL='https://files.pythonhosted.org/packages/d3/4b/ae9037ea22ba94eb2cf267e991384c3444f3e6142fa49923352b4ab73e14/tensorflow_macos-2.14.0-cp311-cp311-macosx_12_0_arm64.whl'
+    ;;
+  'windows')
+    DOWNLOAD_URL='https://files.pythonhosted.org/packages/ad/6e/1bfe367855dd87467564f7bf9fa14f3b17889988e79598bc37bf18f5ffb6/tensorflow_intel-2.14.0-cp311-cp311-win_amd64.whl'
+    ;;
+  *)
+    echo "TensorFlow distribution for ${PLATFORM} is not supported for download"
+    return 1;
+esac
+
+mkdir -p $DOWNLOAD_FOLDER
+
+if [ ! -f $DOWNLOAD_FOLDER/$DOWNLOADED_FILE ]; then
+  curl -L $DOWNLOAD_URL --output-dir $DOWNLOAD_FOLDER --output $DOWNLOADED_FILE
+fi
+
+unzip -q -u -d $DOWNLOAD_FOLDER $DOWNLOAD_FOLDER/$DOWNLOADED_FILE
+
+if [[ "$PLATFORM" =~ "linux" ]]; then
+  ln -fs $DOWNLOAD_FOLDER/tensorflow/libtensorflow_cc.so.2 $DOWNLOAD_FOLDER/tensorflow/libtensorflow_cc.so
+  ln -fs $DOWNLOAD_FOLDER/tensorflow/libtensorflow_framework.so.2 $DOWNLOAD_FOLDER/tensorflow/libtensorflow_framework.so
+
+elif [[ "$PLATFORM" =~ "macosx" ]]; then
+  ln -fs $DOWNLOAD_FOLDER/tensorflow/libtensorflow_cc.2.dylib $DOWNLOAD_FOLDER/tensorflow/libtensorflow_cc.dylib
+  ln -fs $DOWNLOAD_FOLDER/tensorflow/libtensorflow_framework.2.dylib $DOWNLOAD_FOLDER/tensorflow/libtensorflow_framework.dylib
+fi
