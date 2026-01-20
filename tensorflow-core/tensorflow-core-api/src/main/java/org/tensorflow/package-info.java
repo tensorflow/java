f2c044866e758fde81634e26c71edaf1acfbcd65
Shreyansh Shekhar Dwivedi <shreyanshsd512gb@gmail.com>
2026-01-20 16:53:10 +0000

    style: fix formatting via GitHub Codespaces

--- tensorflow-core/tensorflow-core-api/src/main/java/org/tensorflow/package-info.java
+++ tensorflow-core/tensorflow-core-api/src/main/java/org/tensorflow/package-info.java
@@ -16,12 +16,12 @@ limitations under the License.
 /**
  * Defines classes to build, save, load and execute TensorFlow models.
  *
- * <p><b>API Stability</b>: Since version 1.0.0, the TensorFlow Java API is covered by
- * TensorFlow <a href="https://www.tensorflow.org/guide/versions#api_stability">API stability guarantees</a>.
- * Please note that as this library is a wrapper for the TensorFlow C API, its stability is
- * subject to the stability of the underlying upstream TensorFlow project. See the
- * <a href="https://github.com/tensorflow/java/blob/master/README.md">README.md</a>
- * for installation instructions.
+ * <p><b>API Stability</b>: Since version 1.0.0, the TensorFlow Java API is covered by TensorFlow <a
+ * href="https://www.tensorflow.org/guide/versions#api_stability">API stability guarantees</a>.
+ * Please note that as this library is a wrapper for the TensorFlow C API, its stability is subject
+ * to the stability of the underlying upstream TensorFlow project. See the <a
+ * href="https://github.com/tensorflow/java/blob/master/README.md">README.md</a> for installation
+ * instructions.
  *
  * <p>The <a
  * href="https://www.tensorflow.org/code/tensorflow/java/src/main/java/org/tensorflow/examples/LabelImage.java">LabelImage</a>
