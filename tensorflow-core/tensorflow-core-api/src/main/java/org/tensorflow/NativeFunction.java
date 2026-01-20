cf36b38d61f73de57ec9a09a6a724ac36a157278
winne42 <firmen@schoech.de>
2026-01-20 15:20:39 +0100

    Feature/add t bool test (#628)
    * add TBoolTest (mix of TStringTest and NumericTypesTestBase)
    
    * minor cleanup
    
    * add copyright comment, fix comment in test
    
    * make class and test methods public to be aligned with other tests
    
    * fix formatting with mvn spotless:apply
    
    ---------
    
    Co-authored-by: Winfried Gerlach <w.schoech@gmail.com>

--- tensorflow-core/tensorflow-core-api/src/main/java/org/tensorflow/NativeFunction.java
+++ tensorflow-core/tensorflow-core-api/src/main/java/org/tensorflow/NativeFunction.java
@@ -20,9 +20,7 @@ import static org.tensorflow.internal.c_api.global.tensorflow.TF_FunctionToFunct
 
 import com.google.protobuf.InvalidProtocolBufferException;
 import java.util.ArrayDeque;
-import java.util.ArrayList;
 import java.util.Collection;
-import java.util.Collections;
 import java.util.LinkedHashSet;
 import java.util.List;
 import java.util.Map;
@@ -92,7 +90,7 @@ class NativeFunction {
                   }
                 });
       }
-      dependencies = Collections.unmodifiableList(new ArrayList<>(deps));
+      dependencies = List.copyOf(deps);
     }
 
     return dependencies;
