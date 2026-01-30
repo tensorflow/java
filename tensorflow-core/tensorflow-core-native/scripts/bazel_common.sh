dfe2627692b3dfc3c80d348ee47679bb728bf83b
Adam Pocock <adam.pocock@oracle.com>
2026-01-30 14:39:29 -0500

    Working on bazel protobuf generation.

--- tensorflow-core/tensorflow-core-native/scripts/bazel_common.sh
+++ tensorflow-core/tensorflow-core-native/scripts/bazel_common.sh
@@ -2,7 +2,6 @@
 set -eu
 
 # Set generic Bazel build options
-export BAZEL_USE_CPP_ONLY_TOOLCHAIN=1
 export BAZEL_VC="${VCINSTALLDIR:-}"
 if [[ -d $BAZEL_VC ]]; then
     export BUILD_FLAGS="--output_user_root=$(cygpath -w $TMP) build"
@@ -16,7 +15,7 @@ fi
 if [[ "${PLATFORM:-}" == macosx-arm64 ]]; then
   BUILD_FLAGS="$BUILD_FLAGS --config=macos_arm64"
 fi
-BUILD_FLAGS="$BUILD_FLAGS --experimental_repo_remote_exec --python_path="$PYTHON_BIN_PATH" --output_filter=DONT_MATCH_ANYTHING --verbose_failures --distinct_host_configuration=true"
+BUILD_FLAGS="$BUILD_FLAGS --experimental_repo_remote_exec --python_path="$PYTHON_BIN_PATH" --output_filter=DONT_MATCH_ANYTHING --verbose_failures"
 
 export BAZEL_BIN=$(pwd -P)/bazel-bin
 export TENSORFLOW_BIN=$BAZEL_BIN/external/org_tensorflow/tensorflow
